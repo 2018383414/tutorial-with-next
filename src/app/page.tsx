36debb2e5e3eb4a1541bdcfdbffa241fe79c8959
Alice <ora202503@gmail.com>
2025-12-15 20:36:13 +0800

    u

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,23 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { Dino } from "./types";
-import Link from "next/link";
+//import { useEffect, useState } from "react";
+//import { Dino } from "./types";
+//import Link from "next/link";
 
-export default function Home() {
-  const [dinosaurs, setDinosaurs] = useState<Dino[]>([]);
+//export default function Home() {
 
-  useEffect(() => {
-    (async () => {
-      const response = await fetch(`/api/dinosaurs`);
-      const allDinosaurs = await response.json() as Dino[];
-      setDinosaurs(allDinosaurs);
-    })();
-  }, []);
+export default async function Home() {
+  const res = await fetch('http://ip-api.com/json', {
+    cache: 'no-store'
+  });
+
+  const data = await res.json();
 
   return (
-    <main>
-      <h1>Welcome to the Dinosaur app</h1>
-      <p>Click on a dinosaur below to learn more.</p>
-      {dinosaurs.map((dinosaur: Dino) => {
-        return (
-          <Link key={dinosaur.name} className="btn-primary" href={`/${dinosaur.name.toLowerCase()}`}>
-            {dinosaur.name}
-          </Link>
-        );
-      })}
+    <main style={{ padding: 24, fontFamily: 'monospace' }}>
+      <h1>Hello from Deno Deploy 🌍</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </main>
   );
 }
+
