e852a2b37f1d1e2e7f50b128c4048799ce125348
Alice <ora202503@gmail.com>
2025-12-15 20:45:27 +0800

    u

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,3 +1,13 @@
-export function GET() {
-    return Response.json("welcome to the dinosaur API");
+export async function GET() {
+  const res = await fetch('http://ip-api.com/json', {
+    cache: 'no-store'
+  });
+
+  const data = await res.json();
+
+  return Response.json({
+    region: Deno.env.get('DENO_REGION') ?? 'unknown',
+    ipApi: data
+  });
 }
+
